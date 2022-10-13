b2e8fcc89cfb49fed865f9c8646fc5339481d7fc
Dmitry G. Anderson <dandgerson@gmail.com>
2022-10-13 23:46:17 +0300

    some rules for reducers

--- store-example.js
+++ store-example.js
@@ -5,13 +5,31 @@ const storeExample = () => {
     value: 0,
   };
 
-  const reducer = (state, action) => {
+  const INCREMENT = "INCREMENT";
+  const ADD = "ADD";
+
+  const increment = () => ({ type: INCREMENT });
+  const add = (amount) => ({ type: ADD, payload: amount });
+
+  const reducer = (state = initialState, action) => {
+    if (action.type === INCREMENT) {
+      return { value: state.value + 1 };
+    }
+
+    if (action.type === ADD) {
+      return { value: state.value + action.payload };
+    }
+
     return state;
   };
 
   const store = createStore(reducer);
 
   console.log("storeExample", { store });
+
+  store.dispatch(increment());
+
+  console.log(store.getState());
 };
 
 export default storeExample;
